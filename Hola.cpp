f4b5198c7020c9026152a92a33737cc18134f779
fedel8 <81226531+fedel8@users.noreply.github.com>
2022-06-02 13:44:13 +0000

    commit from windows!

--- Hola.cpp
+++ Hola.cpp
@@ -4,7 +4,8 @@
 int main()
 {
 
-std::cout << "Hola, Mundo desde github codespaces!" << std::endl; 
+std::cout << "Hola, Mundo desde github codespaces (desde la tablet!)!" << std::endl; 
+std::cout << "Hola, Mundo desde github codespaces desde windows!" << std::endl; 
 
 return(0);
 }
